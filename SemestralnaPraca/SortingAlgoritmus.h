9ed49d8f9f6dd2dd6c2e7b3bb3b266ff88d4418e
David Kučera <kucera.david2002@gmail.com>
2023-05-15 14:20:14 +0200

    removed unused algos for sorting alphabetically

--- SemestralnaPraca/SortingAlgoritmus.h
+++ SemestralnaPraca/SortingAlgoritmus.h
@@ -21,41 +21,12 @@ public:
 	{
 		ds::adt::ShellSort<Udaj> ss;
 		std::locale::global(std::locale("Slovak_Slovakia.1250"));
-		/*ss.sort(*is, [](auto udaj1, auto udaj2)
-		{
-			auto word1 = udaj1.get_official_title();
-			auto word2 = udaj2.get_official_title();
-			return std::lexicographical_compare(word1.begin(), word1.end(), word2.begin(), word2.end());
-		});*/
 
 		ss.sort(*is, [](auto word1, auto word2)
 		{
 			return word1.get_official_title() < word2.get_official_title();
 		});
-
-		/*ss.sort(*is, [](auto word1, auto word2)
-		{
-			return word2.get_official_title().compare(word1.get_official_title()) > word1.get_official_title().compare(word2.get_official_title());
-		});*/
-
-		/*ss.sort(*is, [](auto word1, auto word2)
-		{
-			const std::string w1 = word1.get_official_title();
-			const std::string w2 = word2.get_official_title();
-			return local_compare(w1, w2);
-		});*/
 	}
-
-	/*static bool local_compare(const std::string& str1, const std::string& str2)
-	{
-		const std::locale loc("Slovak_Slovakia.1250");
-		const auto& col = std::use_facet<std::collate<char>>(loc);
-
-		const char* pb1 = str1.data();
-		const char* pb2 = str2.data();
-
-		return col.compare(pb1, pb1 + str1.size(), pb2, pb2 + str2.size());
-	}*/
 
 	static int count_vowels(const std::string& word)
 	{
